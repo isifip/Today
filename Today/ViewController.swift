bbd8fd2d4ee90829122f936cfc5d4967dba6d17d
Irakli Sokhaneishvili <irakli.swift@gmail.com>
2022-03-16 17:42:53 +0400

    first commit

---
+++ Today/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Today
+//
+//  Created by Irakli Sokhaneishvili on 16.03.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
