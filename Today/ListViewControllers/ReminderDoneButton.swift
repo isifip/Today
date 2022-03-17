132c63d5b04f38febb9d89aab8526024107db8ea
Irakli Sokhaneishvili <irakli.swift@gmail.com>
2022-03-17 17:22:38 +0400

    + Custom button actions

---
+++ Today/ListViewControllers/ReminderDoneButton.swift
@@ -0,0 +1,12 @@
+//
+//  ReminderDoneButton.swift
+//  Today
+//
+//  Created by Irakli Sokhaneishvili on 17.03.22.
+//
+
+import UIKit
+
+class ReminderDoneButton: UIButton {
+    var id: Reminder.ID?
+}
---
+++ Today/ListViewControllers/ReminderListViewController+Actions.swift
@@ -0,0 +1,14 @@
+//
+//  ReminderListViewController+Actions.swift
+//  Today
+//
+//  Created by Irakli Sokhaneishvili on 17.03.22.
+//
+
+import UIKit
+
+extension ReminderListViewController {
+    @objc func didPressDoneButton(_ sender: ReminderDoneButton) {
+        guard let id = sender.id else { return }
+    }
+}
