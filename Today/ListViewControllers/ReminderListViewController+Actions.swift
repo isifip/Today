aee6be14bb63e8fa4b9bf1f71cb657c9eb1eaeaf
Irakli Sokhaneishvili <irakli.swift@gmail.com>
2022-03-17 17:49:18 +0400

    + Wire Target-Action Pair

--- Today/ListViewControllers/ReminderListViewController+Actions.swift
+++ Today/ListViewControllers/ReminderListViewController+Actions.swift
@@ -10,5 +10,6 @@ import UIKit
 extension ReminderListViewController {
     @objc func didPressDoneButton(_ sender: ReminderDoneButton) {
         guard let id = sender.id else { return }
+        completeReminder(with: id)
     }
 }
