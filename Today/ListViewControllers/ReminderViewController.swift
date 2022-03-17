d5bbbe1d2d0d3845abadad4fb5c793b75af8771b
Irakli Sokhaneishvili <irakli.swift@gmail.com>
2022-03-17 18:10:33 +0400

    + ReminderView

---
+++ Today/ListViewControllers/ReminderViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ReminderViewController.swift
+//  Today
+//
+//  Created by Irakli Sokhaneishvili on 17.03.22.
+//
+
+import UIKit
+
+class ReminderViewController: UICollectionViewController {
+    var reminder: Reminder
+    
+    init(reminder: Reminder) {
+        self.reminder = reminder
+        var listConfiguration = UICollectionLayoutListConfiguration(appearance: .insetGrouped)
+        listConfiguration.showsSeparators = false
+        let listLayout = UICollectionViewCompositionalLayout.list(using: listConfiguration)
+        super.init(collectionViewLayout: listLayout)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
