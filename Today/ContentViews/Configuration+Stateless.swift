57c181fe1fa637cc42c064bae658f44ca23610a9
Irakli Sokhaneishvili <irakli.swift@gmail.com>
2022-03-18 18:55:24 +0400

    + Complete the content view

---
+++ Today/ContentViews/Configuration+Stateless.swift
@@ -0,0 +1,15 @@
+//
+//  Configuration+Stateless.swift
+//  Today
+//
+//  Created by Irakli Sokhaneishvili on 18.03.22.
+//
+
+import UIKit
+
+extension UIContentConfiguration {
+    func updated(for state: UIConfigurationState) -> Self {
+        return self
+    }
+}
+
