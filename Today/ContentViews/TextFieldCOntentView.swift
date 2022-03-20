296e82de23be2e711c0bb317c716fcb728dadc1e
Irakli Sokhaneishvili <irakli.swift@gmail.com>
2022-03-20 14:35:32 +0400

    + Make the text configuration editable

--- Today/ContentViews/TextFieldCOntentView.swift
+++ Today/ContentViews/TextFieldCOntentView.swift
@@ -12,7 +12,9 @@ import UIKit
 class TextFieldContentView: UIView, UIContentView {
     
     struct Configuration: UIContentConfiguration {
+        
         var text: String? = ""
+        var onChange: (String) -> Void = { _ in }
         
         func makeContentView() -> UIView & UIContentView {
             return TextFieldContentView(self)
@@ -34,6 +36,8 @@ class TextFieldContentView: UIView, UIContentView {
         self.configuration = configuration
         super.init(frame: .zero)
         addPinnedSubview(textField, insets: UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 16))
+        textField.addTarget(self, action: #selector(didChange(_:)), for: .editingChanged)
+        textField.clearButtonMode = .whileEditing
     }
     
     required init?(coder: NSCoder) {
@@ -43,6 +47,11 @@ class TextFieldContentView: UIView, UIContentView {
     func configure(configuration: UIContentConfiguration) {
         guard let configuration = configuration as? Configuration else { return }
         textField.text = configuration.text
+    }
+    
+    @objc private func didChange(_ sender: UITextField) {
+        guard let configuration = configuration as? TextFieldContentView.Configuration else { return }
+        configuration.onChange(textField.text ?? "")
     }
 }
 
